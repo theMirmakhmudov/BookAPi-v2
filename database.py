c7b2ffc20a74f7624438d5c4c0bb080eafa3343b
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-06-25 13:55:34 +0500

    Commit

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = 'postgresql://postgres:mirmakhmudov@localhost/bookdb'
+env = load_dotenv(".env")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
